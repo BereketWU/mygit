ef581ac1168412746505e64af2bb87f565c79768
BereketWU <139444062+BereketWU@users.noreply.github.com>
2024-04-28 15:00:17 +0300

    Update DoctorPage.js

--- client/src/DoctorPage/DoctorPage.js
+++ client/src/DoctorPage/DoctorPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./DoctorPage.css";
 import Sidebar from "./Sidebar";
+// doctor page
 
 const DoctorPage = () => {
   return (
